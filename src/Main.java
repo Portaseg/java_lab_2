401067fef3a8a2f360a7215708a906ec8c63b34d
PPodwojski <pietrolsek@gmail.com>
2022-01-22 18:27:33 +0100

    lab 2 zad 3

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,37 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę");
+        int a = scan.nextInt();
+        switch (a % 2) {
+            case 0 -> System.out.println("jest parzysta");
+            case 1 -> System.out.println("jest nieparzysta");
+        }
+        switch (a % 7) {
+            case 0 -> System.out.println("jest podzielna przez 7");
+            case 1 -> System.out.println("jest niepodzielna przez 7");
+        }
+
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
-
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0, 0, 0, 0};
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o' -> ++tablica[0];
+                case 'p' -> ++tablica[1];
+                case 'x' -> ++tablica[2];
+                case 'z' -> ++tablica[3];
+                case 'q' -> ++tablica[4];
+                default -> ++tablica[5];
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d- razy\nq: %d- razy\npozostałe: %d - razy",
+                tablica[0], tablica[1], tablica[2],tablica[3],tablica[4],tablica[5]);
 
     }
 }
