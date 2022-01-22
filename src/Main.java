980d0c6bae8d4bae0132b4b0571a756c213231a9
PPodwojski <pietrolsek@gmail.com>
2022-01-22 19:12:21 +0100

    lab 2 zad 4

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,26 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        for (int i = 10; i >= 0 ; i--) {
+            if (i == 5)
+                break;
+            System.out.println("i=" + i);
+        }
+        System.out.println("\n");
+        /*b*/
+        for (int i = 4; i <= 6 ; i++) {
+            if(i*5==25)
+                continue;
+            System.out.println("i="+ i);
+        }
+        System.out.println("\n");
 
+        /*c*/
+        for (int i = 0; i < 20; i++) {
+            if (i == 13)
+                return;
+                System.out.println("i=" + i);
+        }
+        System.out.println("funkcja return nie pozwoli wyświetlic sie tej linijce");
     }
 }
