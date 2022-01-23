6802d3b206677b32e489d59053dcc696597b0ddf
PPodwojski <pietrolsek@gmail.com>
2022-01-23 02:21:53 +0100

    lab 2 zad 6

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,32 @@ public class Main {
                 d) proszę wyświetlić posortowną listę, oraz odwrotniee posortowaną i znów użyć metody z punktu b) do wyświetlenia
                   elementów listy
           */
+        Scanner scan = new Scanner(System.in);
+
+        List<String> lista = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("wpisz zwierzątko do listy: ");
+            String zwierze = scan.next();
+            lista.add(zwierze);
+        }
+
+        System.out.println(lista+"\n");
+
+
+            lista.remove(lista.size() - 1);
+            lista.remove(lista.size() - 1);
+            lista.remove(1); //usuwa drugi element z listy
+
+            for (int i = 0; i < 4; i++) {
+                System.out.println("wpisz ponownie zwierzątko do listy: ");
+                lista.add(scan.next());
+            }
+            read(lista);
+            System.out.println("\nilość elementów listy: " + lista.size() + "\n");
+
+            lista.sort(Collections.reverseOrder());
+            read(lista);
 
 
          /* zad.2 Napisać program który :
@@ -27,15 +53,34 @@ public class Main {
                 b) następnie wyświetlić zawartość set-a w konsoli,
                 c) następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
-        
-        
+
+        Set<Integer> liczby = new TreeSet<>();
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Podaj liczbe: ");
+            liczby.add(scan.nextInt());
+        }
+        System.out.println("zawartosc seta: " + liczby);
+
+        for (Integer liczba : liczby) {
+            System.out.print(liczba + ", ");
+        }
+
+    }
+
+// zad 1
+
+    public static void read(List<String> lista) {
+        for (String element : lista) {
+            System.out.print(element + " ");
+        }
+    }
         /* zad.3* Dla chętnych, nie będzie oceniane:
                 Napisać podobny program jak w zad.1 zamiast zwierząt wprowadzć dowolne dane 
                 np. imiona, nazwiska, miasta, samochody ... . (dowolność),
                 Wykonać na TreeSet 
                 -- podpowiedź: Set<String> nazwaSeta = new TreeSet<>();
-        /*
+        */
 
-    }
 
 }
